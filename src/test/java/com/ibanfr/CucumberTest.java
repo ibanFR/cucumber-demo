764921c3396d855e08c395218c7384335d5a88c5
ivan.fernandez <ivan.fernandez@etu.co>
2023-06-28 12:11:24 +0100

    Integrate Serenity with Cucumber

--- src/test/java/com/ibanfr/CucumberTest.java
+++ src/test/java/com/ibanfr/CucumberTest.java
@@ -1,11 +1,14 @@
 package com.ibanfr;
 
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src/test/resources/features"
+)
 public class CucumberTest {
 }
