195a087f69d48c9d63b99460cd45530519b2d946
ibanFR <ibanfernandezrojo@msn.com>
2024-09-24 13:06:01 +0100

    exclude @wip features from execution

--- src/test/java/com/ibanfr/CucumberTest.java
+++ src/test/java/com/ibanfr/CucumberTest.java
@@ -1,15 +1,14 @@
 package com.ibanfr;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-reports/index.html")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @wip")
 public class CucumberTest {
 }
